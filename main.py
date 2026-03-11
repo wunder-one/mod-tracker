8d486e360ce081ab9fe557ef886aa2d6317e2874
wunder-one <wunderwood@gmail.com>
2026-03-11 10:14:16 -0700

    remove PyQt requirement

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import sys
 import os
-from time import strftime, gmtime
-
-from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtQml import QQmlApplicationEngine
 
 def main():
-    app = QGuiApplication(sys.argv)
 
-    curr_time = strftime("%H:%M:%S", gmtime())
-
-    engine = QQmlApplicationEngine()
-    engine.quit.connect(app.quit)
-    engine.load('./UI/main.qml')
-    engine.rootObjects()[0].setProperty('currTime', curr_time)
 
     sys.exit(app.exec())
 
